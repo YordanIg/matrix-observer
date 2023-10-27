dcb752a4363afffd567729b3e6c337dd342df5c1
Yordan Ignatov <dani.ignatov@gmail.com>
2023-10-27 10:38:46 +0100

    Added missing function required to calculate G matrix

--- src/forward_model.py
+++ src/forward_model.py
@@ -7,6 +7,18 @@ import src.coordinates as CO
 import src.spherical_harmonics as SH
 import src.beam_functions as BF
 
+def calc_summation_matrix(Ntau, Nt):
+    """
+    Crude gain matrix to sum up Nt neighbouring time bins into
+    courser Ntau time bins. i.e. Ntau < Nt
+    """
+    summation_matrix = np.zeros([Ntau, Nt])
+    for i in range(Nt):
+        value = int(i * Ntau / Nt)
+        summation_matrix[value][i] = 1
+
+    return summation_matrix
+
 def calc_averaging_matrix(Ntau, Nt):
     """
     Crude gain matrix to average up Nt neighbouring time bins into
@@ -15,14 +27,15 @@ def calc_averaging_matrix(Ntau, Nt):
     Exploits this trick for dividing each row by its own normalisation
     https://stackoverflow.com/questions/19602187/numpy-divide-each-row-by-a-vector-element
     """
-    summation_matrix = calculate_summation_matrix(Ntau, Nt)
+    summation_matrix = calc_summation_matrix(Ntau, Nt)
 
     #normalise each row to get averaging matrix
     averaging_matrix = summation_matrix / np.sum(summation_matrix, axis=1)[:, None]
 
     return averaging_matrix
 
-def calc_zenith_driftscan_A(nside, lmax, Ntau, lat=-26, lon=0, Nt=np.linspace(0, 24, 24, endpoint=False)):
+def calc_observation_matrix_zenith_driftscan(nside, lmax, Ntau, lat=-26, lon=0, 
+                            Nt=np.linspace(0, 24, 24, endpoint=False)):
     """
     Calculate the total observation and binning matrix A = GPYB
     for a single drifscan antenna pointing at zenith with a cos^2
@@ -32,6 +45,5 @@ def calc_zenith_driftscan_A(nside, lmax, Ntau, lat=-26, lon=0, Nt=np.linspace(0,
     mat_G = calc_averaging_matrix(Ntau=Ntau, lmax=lmax)
     mat_P = CO.calc_pointing_matrix(coords, nside=nside, pixels=False)
     mat_Y = SH.calc_spherical_harmonic_matrix(nside, lmax)
-    mat_B = BF.calc_beam_matrix(nside, lmax, beam_use=beam_cos)
+    mat_B = BF.calc_beam_matrix(nside, lmax, beam_use=BF.beam_cos)
     return mat_G @ mat_P @ mat_Y @ mat_B
-    
