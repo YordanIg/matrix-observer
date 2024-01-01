25c3891937edf4e8d442e796d3c465ba09c56f42
Yordan Ignatov <dani.ignatov@gmail.com>
2024-01-01 14:43:43 +0000

    Adding power law + PCA + 21-cm regression

--- src/powerlaw_regression.py
+++ src/powerlaw_regression.py
@@ -6,6 +6,8 @@ import numpy as np
 from numpy.linalg import inv
 from scipy.linalg import svd
 from scipy.optimize import minimize
+from emcee import EnsembleSampler
+from src.sky_models import cm21_globalT
 
 
 def pca(arr, N):
@@ -97,7 +99,7 @@ def lin_pca_forward_mod(x, mat_X):
         y = X' . theta
     where y is a vector of shape (N,).
 
-    Returns : X (N, Nmod+2)
+    Returns : X' (N, Nmod+2)
     """
     N = len(x)
     Nmod = np.shape(mat_X)[1]
@@ -129,4 +131,99 @@ def lin_pca_regression(x, y, mat_X):
     mat_X_new = lin_pca_forward_mod(x, mat_X)
     return noisefree_linear_regression(y=y, mat_X=mat_X_new)
 
+
+'''def powerlaw_forward_model(theta, x):
+    """
+    Forward model the power law:
+        Ax^(-gamma)
+    for theta = (A, gamma).
+    """
+    amplitude, gamma = theta
+    return amplitude * x **(gamma)
+
+def powerlaw_regression(y, x, theta0):
+    """
+    Find theta = (amplitude, power law index) that zeros:
+        powerlaw_forward_model(theta, x) - y
+    given a starting guess theta0.
+    """
+    to_min = '''
+
+
+
+
+def fg_powerlawPCA_forward_model(nuarr, theta_fg, pca_basis):
+    """
+    Forward model the power law + PCA foreground model, given an (Nfreq, Npca) 
+    basis matrix pca_basis. The vector theta_fg consists of:
+        amplitude, power law index, pca params
+    """
+    assert len(nuarr) == np.shape(pca_basis)[0]
+    assert len(theta_fg) == np.shape(pca_basis)[1]+2
+    amplitude, slope = theta_fg[:2]
+    theta_pca = theta_fg[2:]
+    return amplitude * (nuarr/60) **(slope) * np.exp(slope* pca_basis@theta_pca )
+
+
+def fg_powerlawPCA_regression(nuarr, C0, pca_basis, bounds):
+    """
+    Fit a power law + PCA to data. 
+    """
+    Npca = np.shape(pca_basis)[1]
+    to_max = lambda theta: -np.sum((C0 - fg_powerlawPCA_forward_model(nuarr, theta, pca_basis))**2)
+
+    x0 = [np.exp(40), -5]
+    x0 += [1e-4]*Npca
+
+    nwalkers = 16
+    ndim = 2 + Npca
+
+    pos = np.array([np.random.uniform(*bound, size=nwalkers) for bound in bounds])
+    pos = pos.T
+
+    print(np.shape(pos))
+    sampler = EnsembleSampler(nwalkers=nwalkers, ndim=ndim, log_prob_fn=to_max)
+    sampler.run_mcmc(pos, 10000, progress=True)
+    return sampler
+
+
+def fg_powerlawPCA_cm21mon_forward_model(nuarr, theta, pca_basis):
+    """
+    Forward model the power law + PCA foreground model and the 21-cm gaussian, 
+    given an (Nfreq, Npca) basis matrix pca_basis. The vector theta_fg consists 
+    of:
+        amplitude, power law index, pca params, 21-cm params
+    """
+    assert len(theta) == np.shape(pca_basis)[1] + 2 + 3
+    amplitude, slope = theta[:2]
+    theta_pca = theta[2:-3]
+    gauss_amp = theta[-3]
+    gauss_cent = theta[-2]
+    gauss_width = theta[-1]
+    fg_part = amplitude * (nuarr/60) **(slope) * np.exp(slope* pca_basis@theta_pca )
+    cm21_part = cm21_globalT(nuarr, gauss_amp, gauss_cent, gauss_width)
+    return fg_part+cm21_part
+
+
+def fg_powerlawPCA_cm21mon_regression(nuarr, C0, pca_basis, bounds):
+    """
+    Fit a power law + PCA and a gaussian trough to data. Returns an emcee 
+    sampler that's been run.
+    """
+    Npca = np.shape(pca_basis)[1]
+    to_max = lambda theta: -np.sum((C0 - fg_powerlawPCA_cm21mon_forward_model(nuarr, theta, pca_basis))**2)
+
+    x0 = [np.exp(40), -5]
+    x0 += [1e-4]*Npca
+
+    nwalkers = 16
+    ndim = 2 + Npca + 3
+
+    pos = np.array([np.random.uniform(*bound, size=nwalkers) for bound in bounds])
+    pos = pos.T
+
+    print(np.shape(pos))
+    sampler = EnsembleSampler(nwalkers=nwalkers, ndim=ndim, log_prob_fn=to_max)
+    sampler.run_mcmc(pos, 100000, progress=True)
+    return sampler
 
