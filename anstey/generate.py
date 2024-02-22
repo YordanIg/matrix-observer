415bd268c487879e7ec4694dcca4f0a40b90f538
Yordan Ignatov <dani.ignatov@gmail.com>
2024-02-22 13:26:41 +0000

    Adding a natively lower resolution (nside=16) version of the GSMA.

--- anstey/generate.py
+++ anstey/generate.py
@@ -5,13 +5,20 @@ following the method of Anstey et. al. 2021 [2010.09644].
 
 import numpy as np
 from pygdsm import GlobalSkyModel
+from healpy import ud_grade
 
-def main():
+def main(nside_out=None):
     T_CMB = 2.725
     sky   = GlobalSkyModel()
     T_230, T_408 = sky.generate([230, 408])
+
+    tag=''
+    if nside_out is not None:
+        T_230 = ud_grade(T_230, nside_out)
+        T_408 = ud_grade(T_408, nside_out)
+        tag=f'_{nside_out}'
     indexes = -np.log((T_230-T_CMB)/(T_408-T_CMB)) / np.log(230/408)
-    np.save('/Users/yordani/Documents/boosted_compass/matrix-observer/anstey/indexes', 
+    np.save('/Users/yordani/Documents/boosted_compass/matrix-observer/anstey/indexes'+tag, 
             np.array([T_408, indexes]))
 
 if __name__ == '__main__':
