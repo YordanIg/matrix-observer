0014f9489d5d49d67bd954b923793e140e0a0e7a
Yordan Ignatov <dani.ignatov@gmail.com>
2024-04-08 17:21:34 +0100

    Updated to be in line with the fg+21 cm code's functionality

--- nregions_inference.py
+++ nregions_inference.py
@@ -1,7 +1,8 @@
 """
-Run Nregions power law inference on the degraded GSMA sky. Provides functions 
-to do the forward modelling, inference and plotting of results. Saves chains in:
-    saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}_(0/1).npy
+Run Nregions power law inference on the degraded GSMA sky plus the 21-cm 
+monopole sky. Provides functions to do the forward modelling, inference and 
+plotting of results. Saves chains in:
+    saves/Nregs_pl_gsmalo_cm21mon/{Nregions}reg{noisetag}_(0/1).npy
 where Nregions is the number of regions, noisetag can be either 'unoise' =
 uniform noise or 'radnoise' = radiometric noise. 
 
@@ -9,6 +10,7 @@ main runs inference once, while main_tworun runs it twice, appending _0 and _1
 on the filenames. The first run uses larger errors than the second to zero in on
 the correct posterior position.
 """
+from pickle import dump
 import healpy as hp
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,27 +22,69 @@ import src.sky_models as SM
 from src.spherical_harmonics import RealSphericalHarmonics
 RS = RealSphericalHarmonics()
 
-def fiducial_obs(uniform_noise=False):
+
+# Fiducial frequency array
+Nfreq = 51
+nuarr = np.linspace(50,100,Nfreq)
+
+# Default parameters of the observation and fiducial sky.
+default_pars = {
+    "times"  : np.linspace(0,24,3, endpoint=False),
+    "unoise" : 1,                                         # Uniform noise level in Kelvin (only used if uniform_noise=True).
+    "tint"   : 1,                                         # Total integration time in hours (only used if uniform_noise=False).
+    "lmax"   : 32,
+    "nside"  : 16
+}
+
+def fiducial_obs(uniform_noise=False, unoise_K=None, tint=None, times=None, 
+                 lmax=None, nside=None):
     """
     Forward model the fiducial degraded GSMA.
+
+    Parameters
+    ----------
+    uniform_noise
+        If uniform_noise is True, add gaussian uniform noise with level unoise_K 
+        kelvin. If false, add radiometric noise with tint hours of total
+        integration time.
+    times
+        Observation times in hours. Defaults to default_pars["times"].
+    lmax, nside
+        Lmax and Nside of the fiducial sky. Defults to 
+        default_pars["lmax/nside"].
     """
-    Nfreq = 51
-    nuarr = np.linspace(50,100,Nfreq)
-    lmax = 32
-    nside = 16
-    npix = hp.nside2npix(nside)
+    if unoise_K is None:
+        unoise_K = default_pars["unoise"]
+    if tint is None:
+        tint = default_pars["tint"]
+    if times is None:
+        times = default_pars["times"]
+    if lmax is None:
+        lmax = default_pars["lmax"]
+    if nside is None:
+        nside = default_pars["nside"]
+
     narrow_cosbeam = lambda x: BF.beam_cos(x, theta0=0.8)
     fg_alm = SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax)
 
-    times = np.linspace(0,24,24, endpoint=False)
+    times = np.linspace(0,24,3, endpoint=False)
     mat_A, (mat_G, mat_P, mat_Y, mat_B) = FM.calc_observation_matrix_multi_zenith_driftscan_multifreq(nuarr, nside, lmax, Ntau=len(times), times=times, beam_use=narrow_cosbeam, return_mat=True)
 
-    d = mat_A@fg_alm
+    d = mat_A@(fg_alm)
     if uniform_noise:
-        dnoisy, noise_covar = SM.add_noise_uniform(temps=d, err=1)
+        dnoisy, noise_covar = SM.add_noise_uniform(temps=d, err=unoise_K)
     elif not uniform_noise:
-        dnoisy, noise_covar = SM.add_noise(temps=d, dnu=1, Ntau=npix, t_int=1)
-    return dnoisy, noise_covar, mat_A, mat_Y, nuarr
+        dnoisy, noise_covar = SM.add_noise(temps=d, dnu=1, Ntau=len(times), t_int=tint)
+
+    params = {
+        "uniform_noise" : uniform_noise,
+        "unoise" : unoise_K,
+        "tint"   : tint,
+        "times"  : times,
+        "lmax"   : lmax,
+        "nside"  : nside
+    }
+    return dnoisy, noise_covar, mat_A, mat_Y, params
 
 
 def mask_split(Nregions=9, visualise=False):
@@ -86,7 +130,6 @@ def mask_split(Nregions=9, visualise=False):
     return mask_maps, inference_bounds
 
 
-
 def log_likelihood(theta, y, yerr, model):
     """
     Compute the Gaussian log-likelihood, given a model(theta) and data y
@@ -129,29 +172,56 @@ def log_posterior(theta, y, yerr, model, prior_range):
     return lp
 
 
-def inference(inference_bounds, noise_covar, dnoisy, model, steps=10000, theta_guess=None, tag=''):
+def inference(inference_bounds, noise_covar, dnoisy, model, steps=10000, 
+              theta_fg_guess=None, tag=''):
     """
     Run inference.
     If inference bounds are passed, they will be bisected to find a guess at the 
     starting inference position. If theta_guess is passed, this information is 
     disregarded.
+
+    Parameters
+    ----------
+    inference_bounds
+        The bounds of each power law region. NOT used as priors.
+    noise_covar
+        Noise covariance matrix
+    dnoisy
+        Noisy data
+    model
+        The inference model.
+    steps
+        Number of steps to take in the inference.
+    theta_fg_guess
+        Initial guess of the foreground parameters. Defults to the middle value
+        of inference_bounds.
+    tag
+        Optional tag to add to the chain that's being saved. 
     """
     # create a small ball around the MLE the initialize each walker
-    nwalkers, ndim = 32, len(inference_bounds)
-    if theta_guess is None:
-        theta_guess = np.array([0.5*(bound[0]+bound[1]) for bound in inference_bounds])
-    pos = theta_guess*(1 + 1e-4*np.random.randn(nwalkers, ndim))
-    priors = np.array([[-0.1, 5.0]]*ndim)
+    nwalkers, fg_dim = 32, len(inference_bounds)
+    ndim = fg_dim
 
+    if theta_fg_guess is None:
+        theta_guess = [0.5*(bound[0]+bound[1]) for bound in inference_bounds]
+        theta_guess = np.array(theta_guess)
+    elif theta_fg_guess is not None:
+        theta_guess = np.array(theta_fg_guess)
+    pos = theta_guess*(1 + 1e-4*np.random.randn(nwalkers, ndim))
+
+    priors = [[0.5, 5.0]]*fg_dim
+    priors = np.array(priors)
     # run emcee
     err = np.sqrt(noise_covar.diag)
     sampler = EnsembleSampler(nwalkers, ndim, log_posterior, 
                         args=(dnoisy.vector, err, model, priors))
     _=sampler.run_mcmc(pos, steps, progress=True)
-    np.save(f"saves/Nregs_pl_gsmalo/{ndim}reg{tag}", sampler.get_chain())  # SAVE THE CHAIN.
+    np.save(f"saves/Nregs_pl_gsmalo/{fg_dim}reg{tag}", sampler.get_chain())  # SAVE THE CHAIN.
 
 
-def main(Nregions=6, steps=10000, return_model=False, uniform_noise=True):
+def main(Nregions=6, steps=10000, return_model=False, uniform_noise=True, tag="", 
+        unoise_K=None, tint=None, times=None, lmax=None, nside=None, 
+        theta_fg_guess=None):
     """
     Run Nregions inference on observations of the degraded GSMA, with either 
     uniform or radiometric noise.
@@ -161,16 +231,22 @@ def main(Nregions=6, steps=10000, return_model=False, uniform_noise=True):
     elif not uniform_noise:
         noisetag = '_radnoise'
 
-    dnoisy, noise_covar, mat_A, mat_Y, nuarr = fiducial_obs(uniform_noise=uniform_noise)
+    dnoisy, noise_covar, mat_A, mat_Y, pars = fiducial_obs(uniform_noise, unoise_K, tint, times, lmax, nside)
+    np.save(f"saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}{tag}_data.npy", dnoisy.vector)
+    with open(f"saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}{tag}_pars.pkl", "wb") as f:
+        dump(pars, f)
+
     mask_maps, inference_bounds = mask_split(Nregions=Nregions)
     model = FM.genopt_nregions_pl_forward_model(nuarr=nuarr, masks=mask_maps, observation_mat=mat_A, spherical_harmonic_mat=mat_Y)
     if return_model:
         return model
-    model(theta=np.array([2]*Nregions))
-    inference(inference_bounds, noise_covar, dnoisy, model, steps=steps, tag=noisetag)
+    
+    inference(inference_bounds, noise_covar, dnoisy, model, steps=steps, theta_fg_guess=theta_fg_guess, tag=noisetag)
 
 
-def main_tworun(Nregions=9, steps=10000, uniform_noise=True):
+def main_tworun(Nregions=6, steps=10000, uniform_noise=True, tag="", 
+        unoise_K=None, tint=None, times=None, lmax=None, nside=None, 
+        theta_fg_guess=None):
     """
     Do the same as main, but run inference with larger errors, then with smaller
     errors, starting at the mean inferred parameter position of the prior run.
@@ -180,20 +256,24 @@ def main_tworun(Nregions=9, steps=10000, uniform_noise=True):
     elif not uniform_noise:
         noisetag = '_radnoise'
 
-    dnoisy, noise_covar, mat_A, mat_Y, nuarr = fiducial_obs(uniform_noise=uniform_noise)
+    dnoisy, noise_covar, mat_A, mat_Y, pars = fiducial_obs(uniform_noise, unoise_K, tint, times, lmax, nside)
+    np.save(f"saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}{tag}_data.npy", dnoisy.vector)
+    with open(f"saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}{tag}_pars.pkl", "wb") as f:
+        dump(pars, f)
+
     mask_maps, inference_bounds = mask_split(Nregions=Nregions)
     model = FM.genopt_nregions_pl_forward_model(nuarr=nuarr, masks=mask_maps, observation_mat=mat_A, spherical_harmonic_mat=mat_Y)
-    model(theta=np.array([2]*Nregions))
-    # Run inference the first time.
-    inference(inference_bounds, noise_covar*100, dnoisy, model, steps=steps, tag=f'{noisetag}_0')
 
-    chain = np.load(f"saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}_0.npy")
-    chain = chain[15000:]  # Burn-in.
+    # Run inference the first time.
+    inference(inference_bounds, noise_covar*100, dnoisy, model, steps=steps, theta_fg_guess=theta_fg_guess, tag=f'{noisetag}{tag}_0')
+
+    chain = np.load(f"saves/Nregs_pl_gsmalo/{Nregions}reg{noisetag}{tag}_0.npy")
+    chain = chain[5000:]  # Burn-in.
     ch_sh = np.shape(chain)
     chain_flat = np.reshape(chain, (ch_sh[0]*ch_sh[1], ch_sh[2]))  # Flatten chain.
     theta_guess = np.mean(chain_flat, axis=0)
     # Run inference the second time.
-    inference(inference_bounds, noise_covar, dnoisy, model, steps=steps, theta_guess=theta_guess, tag=f'{noisetag}_1')
+    inference(inference_bounds, noise_covar, dnoisy, model, steps=steps, theta_fg_guess=theta_guess, tag=f'{noisetag}{tag}_1')
 
 
 def plot_non_uniform_noise_comparison():
