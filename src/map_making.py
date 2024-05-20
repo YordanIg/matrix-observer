98400e962d284317c8c056e3e8440256e75a77d0
Yordan Ignatov <dani.ignatov@gmail.com>
2024-05-20 14:23:25 +0100

    Added L2 regularization to the estimator matrix calculator

--- src/map_making.py
+++ src/map_making.py
@@ -4,7 +4,7 @@ Reversing the forward-modelling process using maximum-likelihood methods.
 import numpy as np
 from src.blockmat import BlockMatrix
 
-def calc_ml_estimator_matrix(mat_A, mat_N, cov=False):
+def calc_ml_estimator_matrix(mat_A, mat_N, cov=False, delta=None):
     """
     For the general problem 
         d = Aa + n
@@ -17,22 +17,35 @@ def calc_ml_estimator_matrix(mat_A, mat_N, cov=False):
 
     If cov is True, also returns the covariance matrix of \hat{a}_{ml} 
     (the "map"), given by C_N = [ A^{T} N^{-1} A ]^{-1}.
+
+    If delta is passed, will L2 regularize by calculating
+        W = [ A^{T} N^{-1} A + delta^2]^{-1} A^{T} N^{-1}
+    instead.
     """
     block_mats = False
     if isinstance(mat_A, BlockMatrix) and isinstance(mat_N, BlockMatrix):
         block_mats = True
     elif isinstance(mat_A, BlockMatrix) or isinstance(mat_N, BlockMatrix):
         raise TypeError(f"mat_A and mat_B must either both be ndarrays or BlockMatrix, but are {type(mat_A)} and {type(mat_N)}")
+    
     if block_mats:
         inv_mat_N = mat_N.inv
     else:
         inv_mat_N = np.linalg.inv(mat_N)
+    
     inv_map_covar = mat_A.T @ inv_mat_N @ mat_A
+
+    # L2 regularization.
+    if delta is not None:
+        inv_map_covar += delta**2 *np.identity(len(inv_map_covar))
+    
     if block_mats:
         map_covar = inv_map_covar.inv
     else:
         map_covar = np.linalg.inv(inv_map_covar)
+
     mat_W = map_covar @ mat_A.T @ inv_mat_N
+
     if cov:
         return mat_W, map_covar
     return mat_W
