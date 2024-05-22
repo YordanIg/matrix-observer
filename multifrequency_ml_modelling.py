60494db15e35bb4696f3175aaf03ee9e339798cd
Yordan Ignatov <dani.ignatov@gmail.com>
2024-05-22 17:09:41 +0100

    a memory-optimized version of nontrivial_obs

--- multifrequency_ml_modelling.py
+++ multifrequency_ml_modelling.py
@@ -60,13 +60,19 @@ def trivial_obs():
     # Generate observation matrix
     mat_A = FM.calc_observation_matrix_all_pix(nside, lmax, Ntau=npix, beam_use=narrow_cosbeam)
     mat_A = BlockMatrix(mat=mat_A, mode='block', nblock=len(nuarr))
+    if lmax != lmod:
+        mat_A_mod = FM.calc_observation_matrix_all_pix(nside, lmod, Ntau=npix, beam_use=narrow_cosbeam)
+        mat_A_mod = BlockMatrix(mat=mat_A_mod, mode='block', nblock=len(nuarr))
+    else:
+        mat_A_mod = mat_A
 
     # Perform fiducial observations
     d = mat_A @ fid_alm
     dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=npix, t_int=1e4)
 
     # Reconstruct the max likelihood estimate of the alm
-    mat_W   = MM.calc_ml_estimator_matrix(mat_A, noise_covar)
+    _ = MM.calc_ml_estimator_matrix(mat_A=mat_A_mod.block[-1], mat_N=noise_covar.block[-1], cond=True)
+    mat_W   = MM.calc_ml_estimator_matrix(mat_A_mod, noise_covar)
     rec_alm = mat_W @ dnoisy
 
     # Extract the monopole component of the reconstructed alm.
@@ -124,7 +130,7 @@ def nontrivial_obs():
 
     # Perform fiducial observations
     d = mat_A @ fid_alm
-    dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=npix, t_int=1e4)
+    dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=len(times), t_int=1e4)
 
     # Reconstruct the max likelihood estimate of the alm
     mat_W   = MM.calc_ml_estimator_matrix(mat_A_mod, noise_covar)
@@ -156,3 +162,129 @@ def nontrivial_obs():
     plt.xlabel("Frequency [MHz]")
     plt.show()
 
+
+def nontrivial_obs_memopt():
+    """
+    A memory-friendly version of nontrivial_obs which computes the reconstruction
+    of each frequency seperately, then brings them all together.
+    """
+    # Model and observation params
+    nside   = 32
+    lmax    = 32
+    lmod    = lmax
+    delta   = 1e-3
+    Nlmax   = RS.get_size(lmax)
+    Nlmod   = RS.get_size(lmod)
+    npix    = hp.nside2npix(nside)
+    lats = np.array([-26*2, -26, 26, 26*2])#np.linspace(-80, 80, 100)#
+    times = np.linspace(0, 24, 24, endpoint=False)
+    nuarr   = np.linspace(50,100,51)
+    cm21_params     = (-0.2, 80.0, 5.0)
+    narrow_cosbeam  = lambda x: BF.beam_cos(x, 0.8)
+
+    # Generate foreground and 21-cm signal alm
+    fg_alm   = SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax, nside=nside, use_mat_Y=True)
+    cm21_alm = SM.cm21_gauss_mon_alm(nu=nuarr, lmax=lmax, params=cm21_params)
+    fid_alm  = fg_alm + cm21_alm
+
+    # Generate observation matrix for the modelling and for the observations.
+    mat_A = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmax, lats=lats, times=times, beam_use=narrow_cosbeam)
+    mat_A = BlockMatrix(mat=mat_A, mode='block', nblock=len(nuarr))
+    mat_A_mod = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmod, lats=lats, times=times, beam_use=narrow_cosbeam)
+    mat_A_mod = BlockMatrix(mat=mat_A_mod, mode='block', nblock=len(nuarr))
+    
+    # Perform fiducial observations
+    d = mat_A @ fid_alm
+    dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=len(times), t_int=2500, seed=456)#t_int=100, seed=456)#
+
+
+    # Reconstruct the max likelihood estimate of the alm
+    mat_Ws   = [MM.calc_ml_estimator_matrix(mat_A_mod_block, noise_covar_block, delta=delta, cond=True) for mat_A_mod_block, noise_covar_block in zip(mat_A_mod.block, noise_covar.block)]
+    mat_W = BlockMatrix(mat=np.array(mat_Ws))
+    rec_alm = mat_W @ dnoisy
+
+    # Extract the monopole component of the reconstructed alm.
+    fid_a00  = np.array(fid_alm[::Nlmax])
+    fg_a00  = np.array(fg_alm[::Nlmax])
+    rec_a00 = np.array(rec_alm.vector[::Nlmod])
+
+    # Fit the reconstructed a00 component with a polynomial and 21-cm gaussian
+    fg_mon_p0 = [15, 2.5, .001]
+    cm21_mon_p0 = [-0.2, 80, 5]
+    res = curve_fit(f=fg_cm21_polymod, xdata=nuarr, ydata=rec_a00, p0=fg_mon_p0+cm21_mon_p0)
+    
+    plt.plot(nuarr, rec_a00-fid_a00, label='$a_{00}$ reconstructed - $a_{00}$ fid fg')
+    plt.axhline(y=0, linestyle=":", color='k')
+    plt.legend()
+    plt.ylabel("Temperature [K]")
+    plt.xlabel("Frequency [MHz]")
+    plt.show()
+
+    # Plot everything
+    plt.plot(nuarr, cm21_mod(nuarr, *res[0][-3:]), label='fit 21-cm monopole')
+    #plt.plot(nuarr, rec_a00-fg_a00, label='$a_{00}$ reconstructed - fid fg')
+    plt.plot(nuarr, cm21_mod(nuarr, *cm21_mon_p0), label='fiducial 21-cm monopole', linestyle=':', color='k')
+    plt.legend()
+    plt.ylabel("Temperature [K]")
+    plt.xlabel("Frequency [MHz]")
+    plt.show()
+
+    plt.plot(nuarr, cm21_mod(nuarr, *res[0][-3:])-cm21_mod(nuarr, *cm21_mon_p0), label='fit 21-cm monopole')
+    plt.plot(nuarr, rec_a00-fg_a00-cm21_mod(nuarr, *cm21_mon_p0), label='$a_{00}$ reconstructed - fid fg')
+    plt.plot(nuarr, cm21_mod(nuarr, *cm21_mon_p0)-cm21_mod(nuarr, *cm21_mon_p0), label='fiducial 21-cm monopole', linestyle=':', color='k')
+    plt.legend()
+    plt.ylabel("Temperature [K]")
+    plt.xlabel("Frequency [MHz]")
+    plt.show()
+
+
+def nontrivial_fg_obs_memopt():
+    """
+    A memory-friendly version of nontrivial_obs which computes the reconstruction
+    of each frequency seperately, then brings them all together.
+
+    This one only reconstructs foregrounds.
+    """
+    # Model and observation params
+    nside   = 32
+    lmax    = 32
+    lmod    = 32
+    delta   = 1e-3
+    Nlmax   = RS.get_size(lmax)
+    Nlmod   = RS.get_size(lmod)
+    npix    = hp.nside2npix(nside)
+    lats = np.linspace(-80, 80, 100)#np.array([-26*2, -26, 26, 26*2])#
+    times = np.linspace(0, 24, 24, endpoint=False)
+    nuarr   = np.linspace(50,100,51)
+    cm21_params     = (-0.2, 80.0, 5.0)
+    narrow_cosbeam  = lambda x: BF.beam_cos(x, 0.8)
+
+    # Generate foreground and 21-cm signal alm
+    fg_alm   = SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax, nside=nside, use_mat_Y=True)
+
+    # Generate observation matrix for the modelling and for the observations.
+    mat_A = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmax, lats=lats, times=times, beam_use=narrow_cosbeam)
+    mat_A = BlockMatrix(mat=mat_A, mode='block', nblock=len(nuarr))
+    mat_A_mod = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmod, lats=lats, times=times, beam_use=narrow_cosbeam)
+    mat_A_mod = BlockMatrix(mat=mat_A_mod, mode='block', nblock=len(nuarr))
+    
+    # Perform fiducial observations
+    d = mat_A @ fg_alm
+    dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=len(times), t_int=1e2, seed=123)
+
+
+    # Reconstruct the max likelihood estimate of the alm
+    mat_Ws   = [MM.calc_ml_estimator_matrix(mat_A_mod_block, noise_covar_block, delta=delta) for mat_A_mod_block, noise_covar_block in zip(mat_A_mod.block, noise_covar.block)]
+    mat_W = BlockMatrix(mat=np.array(mat_Ws))
+    rec_alm = mat_W @ dnoisy
+
+    # Extract the monopole component of the reconstructed alm.
+    fg_a00  = np.array(fg_alm[::Nlmax])
+    rec_a00 = np.array(rec_alm.vector[::Nlmod])
+
+    plt.plot(nuarr, rec_a00-fg_a00, label='$a_{00}$ reconstructed - $a_{00}$ fid fg')
+    plt.axhline(y=0, linestyle=":", color='k')
+    plt.legend()
+    plt.ylabel("Temperature [K]")
+    plt.xlabel("Frequency [MHz]")
+    plt.show()
