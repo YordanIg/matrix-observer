9b5a5e1ee426028fab7337f6eb0e41f6df51a8af
Yordan Ignatov <dani.ignatov@gmail.com>
2024-06-07 16:08:33 +0100

    Added exponential regularization

--- src/map_making.py
+++ src/map_making.py
@@ -4,7 +4,55 @@ Reversing the forward-modelling process using maximum-likelihood methods.
 import numpy as np
 from src.blockmat import BlockMatrix
 
-def calc_ml_estimator_matrix(mat_A, mat_N, cov=False, delta=None, cond=False):
+def calc_reg_matrix_exp(Nlmod, nuarr, dnu=1, pow=2.5):
+    """
+    Calculate the regularization matrix B from that textbook, but made for 
+    exponentials.
+    """
+    # Make a single block of the matrix.
+    gammas = [1-dnu*pow/nu for nu in nuarr]
+    gammas.pop()
+    ones = [1.]*(len(nuarr)-2)
+    final_column = np.zeros(len(nuarr)-1)
+    final_column[-1] = 1.
+    square_block = -np.diag(gammas) + np.diag(ones, k=1)
+    block = np.c_[square_block, final_column]
+    
+    # Construct the block matrix.
+    return BlockMatrix(mat=block, mode='block', nblock=Nlmod).matrix
+
+
+def sort_alm_vector_frequency(a, Nfreq, Nlmod):
+    """
+    Sorts an alm vector from an ordering of 
+        (a00(freq1), a1-1(freq1), ..., a00(freq2), ...)
+    to an ordering
+        (a00(freq1), a00(freq2), ..., a1-1(freq1), ...)
+    i.e. from blocks of different l and m modes to blocks of different 
+    frequencies.
+    """
+    assert len(a) == Nfreq*Nlmod
+    sorted_a = [a[i::Nlmod] for i in range(Nlmod)]
+    return np.array(sorted_a).flatten()
+
+
+def sort_alm_vector_lm(a, Nfreq, Nlmod):
+    """
+    Sorts an alm vector from an ordering of 
+        (a00(freq1), a00(freq2), ..., a1-1(freq1), ...)
+    to an ordering
+        (a00(freq1), a1-1(freq1), ..., a00(freq2), ...)
+    i.e. from blocks of different frequencies to blocks of different l and m 
+    modes.
+    """
+    assert len(a) == Nfreq*Nlmod
+    sorted_a = []
+    for i in range(Nfreq):
+        sorted_a += list(a[i::Nfreq])
+    return np.array(sorted_a)
+
+
+def calc_ml_estimator_matrix(mat_A, mat_N, cov=False, delta=None, reg='L2', nuarr=None, cond=False, pow=None):
     """
     For the general problem 
         d = Aa + n
@@ -28,6 +76,7 @@ def calc_ml_estimator_matrix(mat_A, mat_N, cov=False, delta=None, cond=False):
     block_mats = False
     if isinstance(mat_A, BlockMatrix) and isinstance(mat_N, BlockMatrix):
         block_mats = True
+        Nlmod = mat_A.block_shape[1]
     elif isinstance(mat_A, BlockMatrix) or isinstance(mat_N, BlockMatrix):
         raise TypeError(f"mat_A and mat_B must either both be ndarrays or BlockMatrix, but are {type(mat_A)} and {type(mat_N)}")
     
@@ -38,9 +87,26 @@ def calc_ml_estimator_matrix(mat_A, mat_N, cov=False, delta=None, cond=False):
     
     inv_map_covar = mat_A.T @ inv_mat_N @ mat_A
 
-    # L2 regularization.
+    # Regularization.
     if delta is not None:
-        inv_map_covar += delta**2 *np.identity(len(inv_map_covar))
+        if reg=='L2':
+            cost_mat_H = np.identity(len(inv_map_covar))
+        elif reg=='exp':
+            if block_mats:
+                print("Exponential regularization - switching from BlockMats to ndarrays.")
+                block_mats    = False
+                inv_map_covar = inv_map_covar.matrix
+                mat_A = mat_A.matrix
+                inv_mat_N = inv_mat_N.matrix
+
+            # Calculate the B cost matrix.
+            cost_mat_B = calc_reg_matrix_exp(Nlmod=Nlmod, nuarr=nuarr, dnu=nuarr[1]-nuarr[0], pow=pow)
+            # Permute it to take it from alm blocks to frequency blocks.
+            cost_mat_B = sort_alm_vector_lm(a=cost_mat_B.T, Nfreq=len(nuarr), Nlmod=Nlmod).T
+            cost_mat_H = cost_mat_B.T @ cost_mat_B
+        else:
+            raise ValueError
+        inv_map_covar += delta**2 * cost_mat_H
     
     if cond:
         if block_mats:
