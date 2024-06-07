b291d01a34e188d5ae06b3c84978231ea02d3860
Yordan Ignatov <dani.ignatov@gmail.com>
2024-06-07 16:27:43 +0100

    added a version of nontrivial obs that uses ndarrays instead of block matrices. It's intended to be used with exponential regularization

--- multifrequency_ml_modelling.py
+++ multifrequency_ml_modelling.py
@@ -103,7 +103,6 @@ def trivial_obs():
     plt.xlabel("Frequency [MHz]")
     plt.show()
 
-
 def nontrivial_obs():
     # Model and observation params
     nside   = 32
@@ -162,6 +161,142 @@ def nontrivial_obs():
     plt.legend()
     plt.ylabel("Temperature [K]")
     plt.xlabel("Frequency [MHz]")
+    plt.show()
+
+
+def nontrivial_obs_ndarrays():
+    # Model and observation params
+    nside   = 32
+    lmax    = 32
+    lmod    = 8
+    Nlmax   = RS.get_size(lmax)
+    Nlmod   = RS.get_size(lmod)
+    npix    = hp.nside2npix(nside)
+    delta   = 1e-2
+    lats = np.array([-26*2, -26, 26, 26*2])#np.linspace(-80, 80, 100)
+    times = np.linspace(0, 24, 24, endpoint=False)
+    nuarr   = np.linspace(50,100,51)
+    cm21_params     = (-0.2, 80.0, 5.0)
+    narrow_cosbeam  = lambda x: BF.beam_cos(x, 0.8)
+
+    # Generate foreground and 21-cm signal alm
+    fg_alm   = SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax, nside=nside, use_mat_Y=True)
+    cm21_alm = SM.cm21_gauss_mon_alm(nu=nuarr, lmax=lmax, params=cm21_params)
+    fid_alm  = fg_alm + cm21_alm
+
+    # Generate observation matrix for the modelling and for the observations.
+    mat_A = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmax, lats=lats, times=times, beam_use=narrow_cosbeam)
+    mat_A = BlockMatrix(mat=mat_A, mode='block', nblock=len(nuarr))
+    mat_A_mod = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmod, lats=lats, times=times, beam_use=narrow_cosbeam)
+    mat_A_mod = BlockMatrix(mat=mat_A_mod, mode='block', nblock=len(nuarr))
+    
+
+    # Perform fiducial observations
+    d = mat_A @ fid_alm
+    dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=len(times), t_int=1e4)
+
+    # Reconstruct the max likelihood estimate of the alm
+    mat_W, cov  = MM.calc_ml_estimator_matrix(mat_A_mod, noise_covar, cov=True, delta=delta, reg='exp', nuarr=nuarr, pow=2.6)
+    alm_error = np.sqrt(np.diag(cov))
+    rec_alm = mat_W @ dnoisy.vector
+
+    # Extract the monopole component of the reconstructed alm.
+    fg_a00  = np.array(fg_alm[::Nlmax])
+    rec_a00 = np.array(rec_alm[::Nlmod])
+
+    # Fit the reconstructed a00 component with a polynomial and 21-cm gaussian
+    fg_mon_p0 = [15, 2.5, .001]
+    cm21_mon_p0 = [-0.2, 80, 5]
+    res = curve_fit(f=fg_cm21_polymod, xdata=nuarr, ydata=rec_a00, p0=fg_mon_p0+cm21_mon_p0)
+    '''
+    # Compute the chi-square and compare it to the length of the data vector.
+    chi_sq = (dnoisy - mat_A_mod@rec_alm).T @ noise_covar.inv.matrix @ (dnoisy - mat_A_mod@rec_alm)
+    chi_sq = sum(chi_sq.diag)
+    print("Chi-square:", chi_sq, "len(data):", dnoisy.vec_len,"+/-", np.sqrt(2*dnoisy.vec_len), "Nparams:", Nlmod*len(nuarr))
+    '''
+    # Extract the monopole component of the reconstructed alm.
+    fid_a00  = np.array(fid_alm[::Nlmax])
+    fg_a00  = np.array(fg_alm[::Nlmax])
+    rec_a00 = np.array(rec_alm[::Nlmod])
+    a00_error = np.array(alm_error[::Nlmod])
+
+    # Fit the reconstructed a00 component with a polynomial and 21-cm gaussian
+    Npoly = 3
+    fg_mon_p0 = [15, 2.5]
+    fg_mon_p0 += [.001]*(Npoly-2)
+    cm21_mon_p0 = [-0.2, 80, 5]
+    res = curve_fit(f=fg_cm21_polymod, xdata=nuarr, ydata=rec_a00, sigma=a00_error, p0=fg_mon_p0+cm21_mon_p0)
+    
+    plt.plot(nuarr, rec_a00-fid_a00, label='$a_{00}$ reconstructed - $a_{00}$ fid fg')
+    plt.axhline(y=0, linestyle=":", color='k')
+    plt.legend()
+    plt.ylabel("Temperature [K]")
+    plt.xlabel("Frequency [MHz]")
+    plt.show()
+
+    # Provide a corner plot for the 21-cm inference.
+    # Draw samples from the likelihood.
+    chain = np.random.multivariate_normal(mean=res[0][-3:], cov=res[1][-3:,-3:], size=100000)
+    c = ChainConsumer()
+    c.add_chain(chain, parameters=['A', 'nu0', 'dnu'])
+    f = c.plotter.plot()
+    plt.show()
+
+    # Evaluate the model at 100 points drawn from the chain to get 1sigma 
+    # inference bounds in data space.
+    cm21_a00_mod = lambda nuarr, theta: np.sqrt(4*np.pi)*SM.cm21_globalT(nuarr, *theta)
+    chain_samples = np.random.multivariate_normal(mean=res[0][-3:], cov=res[1][-3:,-3:], size=100)
+    cm21_a00_sample_list = [cm21_a00_mod(nuarr, theta) for theta in chain_samples]
+    cm21_a00_sample_mean = np.mean(cm21_a00_sample_list, axis=0)
+    cm21_a00_sample_std = np.std(cm21_a00_sample_list, axis=0)
+
+    # Plot the model evaluated 1 sigma regions and the fiducial monopole.
+    cm21_a00 = np.array(cm21_alm[::Nlmax])
+    plt.plot(nuarr, cm21_a00, label='fiducial', linestyle=':', color='k')
+    plt.fill_between(
+        nuarr,
+        cm21_a00_sample_mean-cm21_a00_sample_std, 
+        cm21_a00_sample_mean+cm21_a00_sample_std,
+        color='C1',
+        alpha=0.8,
+        edgecolor='none',
+        label="inferred"
+    )
+    plt.fill_between(
+        nuarr,
+        cm21_a00_sample_mean-2*cm21_a00_sample_std, 
+        cm21_a00_sample_mean+2*cm21_a00_sample_std,
+        color='C1',
+        alpha=0.4,
+        edgecolor='none'
+    )
+    plt.xlabel("Frequency [MHz]")
+    plt.ylabel("21-cm a00 [K]")
+    plt.legend()
+    plt.show()
+
+    # Do the same thing but take the residuals.
+    plt.plot(nuarr, cm21_a00-cm21_a00, label='fiducial', linestyle=':', color='k')
+    plt.fill_between(
+        nuarr,
+        cm21_a00_sample_mean-cm21_a00_sample_std-cm21_a00, 
+        cm21_a00_sample_mean+cm21_a00_sample_std-cm21_a00,
+        color='C1',
+        alpha=0.8,
+        edgecolor='none',
+        label="inferred"
+    )
+    plt.fill_between(
+        nuarr,
+        cm21_a00_sample_mean-2*cm21_a00_sample_std-cm21_a00, 
+        cm21_a00_sample_mean+2*cm21_a00_sample_std-cm21_a00,
+        color='C1',
+        alpha=0.4,
+        edgecolor='none'
+    )
+    plt.xlabel("Frequency [MHz]")
+    plt.ylabel("Inferred 21-cm a00 residuals [K]")
+    plt.legend()
     plt.show()
 
 
