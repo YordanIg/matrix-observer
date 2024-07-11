edee636b4125c0738f63a4b5c03b42ba0875502f
Yordan Ignatov <dani.ignatov@gmail.com>
2024-07-11 13:27:09 +0100

    moved fiducial obs into a seperate src file called observing (in a way that won't break my other code)

--- nregions_inference.py
+++ nregions_inference.py
@@ -20,96 +20,9 @@ from chainconsumer import ChainConsumer
 import src.forward_model as FM
 import src.beam_functions as BF
 import src.sky_models as SM
+from src.observing import fiducial_obs, default_pars, Nfreq, nuarr
 from src.spherical_harmonics import RealSphericalHarmonics
 RS = RealSphericalHarmonics()
-
-
-# Fiducial frequency array
-Nfreq = 51
-nuarr = np.linspace(50,100,Nfreq)
-
-# Default parameters of the observation and fiducial sky.
-default_pars = {
-    "times"  : np.linspace(0,6,3, endpoint=False),
-    "unoise" : 1,                                         # Uniform noise level in Kelvin (only used if uniform_noise=True).
-    "tint"   : 1,                                         # Total integration time in hours (only used if uniform_noise=False).
-    "lmax"   : 32,
-    "nside"  : 16,
-    "lats"   : [-26]
-}
-
-def fiducial_obs(uniform_noise=False, unoise_K=None, tint=None, times=None, 
-                 Ntau=None, lmax=None, nside=None, lats=None, delta=None, 
-                 chrom=False, cm21_pars=None):
-    """
-    Forward model the fiducial degraded GSMA.
-
-    Parameters
-    ----------
-    uniform_noise
-        If uniform_noise is True, add gaussian uniform noise with level unoise_K 
-        kelvin. If false, add radiometric noise with tint hours of total
-        integration time.
-    times
-        Observation times in hours. Defaults to default_pars["times"].
-    Ntau
-        Number of observation bins. Defaults to len(times).
-    lmax, nside
-        Lmax and Nside of the fiducial sky. Defults to 
-        default_pars["lmax/nside"].
-    delta
-        Gaussian width of uncertainty to add to the default GSMA indices - used
-        to generate different "instances" of the foregrounds. Leave as None to 
-        use the default GSMA.
-    cm21_pars
-        The Gaussian monopole parameters (A, nu0, dnu). If None, no monopole is
-        included.
-    """
-    if unoise_K is None:
-        unoise_K = default_pars["unoise"]
-    if tint is None:
-        tint = default_pars["tint"]
-    if times is None:
-        times = default_pars["times"]
-    if Ntau is None:
-        Ntau = len(times)
-    if lmax is None:
-        lmax = default_pars["lmax"]
-    if nside is None:
-        nside = default_pars["nside"]
-    if lats is None:
-        lats = default_pars["lats"]
-
-    narrow_cosbeam = lambda x: BF.beam_cos(x, theta0=0.8)
-    fid_alm, og_map = SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax, use_mat_Y=True, nside=nside, original_map=True, delta=delta)
-    if cm21_pars is not None:
-        Nlmax = RS.get_size(lmax)
-        cm21_a00 = np.sqrt(4*np.pi)*SM.cm21_globalT(nuarr, *cm21_pars)
-        fid_alm[::Nlmax] += cm21_a00
-
-    if not chrom:
-        mat_A, (mat_G, mat_P, mat_Y, mat_B) = FM.calc_observation_matrix_multi_zenith_driftscan_multifreq(nuarr=nuarr, nside=nside, lmax=lmax, Ntau=Ntau, lats=lats, times=times, beam_use=narrow_cosbeam, return_mat=True)
-    elif chrom:
-        mat_A, (mat_G, mat_P, mat_Y, mat_B) = FM.calc_observation_matrix_multi_zenith_driftscan_chromatic(nuarr, nside, lmax, Ntau, lats, times, beam_use=BF.beam_cos_FWHM, chromaticity=BF.fwhm_func_tauscher, return_mat=True)
-    
-
-    d = mat_A@(fid_alm)
-    if uniform_noise:
-        dnoisy, noise_covar = SM.add_noise_uniform(temps=d, err=unoise_K)
-    elif not uniform_noise:
-        dnoisy, noise_covar = SM.add_noise(temps=d, dnu=1, Ntau=Ntau, t_int=tint)
-
-    params = {
-        "uniform_noise" : uniform_noise,
-        "unoise" : unoise_K,
-        "tint"   : tint,
-        "times"  : times,
-        "Ntau"   : Ntau,
-        "lmax"   : lmax,
-        "nside"  : nside,
-        "og" : og_map
-    }
-    return dnoisy, noise_covar, mat_A, mat_Y, params
 
 
 def mask_split(Nregions=9, visualise=False, nside=None):
