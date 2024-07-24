def8b8e9475e84ed4b87d208ffc9d2da3140acf8
yordani <dani.ignatov@gmail.com>
2024-07-24 13:26:43 +0100

    actual valid working implementation of missing modes modelling! nontrivial_obs_memopt_missing_modes

--- multifrequency_ml_modelling.py
+++ multifrequency_ml_modelling.py
@@ -15,7 +15,7 @@ import src.forward_model as FM
 import src.sky_models as SM
 import src.map_making as MM
 import src.plotting as PL
-from src.blockmat import BlockMatrix
+from src.blockmat import BlockMatrix, BlockVector
 from anstey.generate import T_CMB
 
 RS = SH.RealSphericalHarmonics()
@@ -293,6 +293,103 @@ def nontrivial_obs_memopt(chrom=None, missing_modes=False, reg_delta=None):
         _plot_results(nuarr, Nlmax, Nlmod, rec_alm.vector, alm_error, fid_alm, cm21_alm, res)
     elif reg_delta is not None:
         _plot_results(nuarr, Nlmax, Nlmod, rec_alm, alm_error, fid_alm, cm21_alm, res)
+
+
+def nontrivial_obs_memopt_missing_modes(chrom=None, basemap_err=5):
+    """
+    A memory-friendly version of nontrivial_obs which computes the reconstruction
+    of each frequency seperately, then brings them all together.
+    """
+    # Model and observation params
+    nside   = 32
+    lmax    = 32
+    lmod    = 3
+    Nlmax   = RS.get_size(lmax)
+    Nlmod   = RS.get_size(lmod)
+    delta = SM.basemap_err_to_delta(percent_err=basemap_err)
+    lats = np.array([-26*2, -26, 26, 26*2])#np.linspace(-80, 80, 100)#
+    times = np.linspace(0, 24, 12, endpoint=False)#np.linspace(0, 24, 144, endpoint=False)  # 144 = 10 mins per readout
+    nuarr = np.linspace(50,100,51)
+    cm21_params     = (-0.2, 80.0, 5.0)
+    narrow_cosbeam  = lambda x: BF.beam_cos(x, 0.8)
+
+    # Generate foreground and 21-cm signal alm
+    fg_alm   = SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax, nside=nside, use_mat_Y=True)
+    cm21_alm = SM.cm21_gauss_mon_alm(nu=nuarr, lmax=lmax, params=cm21_params)
+    fid_alm  = fg_alm + cm21_alm
+
+    # Generate observation matrix for the modelling and for the observations.
+    if chrom is not None:
+        if not isinstance(chrom, bool):
+            chromfunc = partial(BF.fwhm_func_tauscher, c=chrom)
+        else:
+            chromfunc = BF.fwhm_func_tauscher
+        print("here")
+        mat_A = FM.calc_observation_matrix_multi_zenith_driftscan_chromatic(nuarr, nside, lmax, lats=lats, times=times, return_mat=False, beam_use=BF.beam_cos_FWHM, chromaticity=chromfunc)
+        mat_A_mod = FM.calc_observation_matrix_multi_zenith_driftscan_chromatic(nuarr, nside, lmod, lats=lats, times=times, return_mat=False, beam_use=BF.beam_cos_FWHM, chromaticity=chromfunc)
+        print("there")
+    elif chrom is None:
+        mat_A = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmax, lats=lats, times=times, beam_use=narrow_cosbeam, return_mat=False)
+        mat_A = BlockMatrix(mat=mat_A, mode='block', nblock=len(nuarr))
+        mat_A_mod = FM.calc_observation_matrix_multi_zenith_driftscan(nside, lmod, lats=lats, times=times, beam_use=narrow_cosbeam, return_mat=False)
+        mat_A_mod = BlockMatrix(mat=mat_A_mod, mode='block', nblock=len(nuarr))
+    
+    # Perform fiducial observations
+    d = mat_A @ fid_alm
+    dnoisy, noise_covar = SM.add_noise(d, 1, Ntau=len(times), t_int=100, seed=456)#t_int=100, seed=456)#
+    sample_noise = np.sqrt(noise_covar.block[0][0,0])
+    print(f"Data generated with noise {sample_noise} K at 50 MHz in the first bin")
+
+    # Generate a missing-modes correction.
+    # Step 1: Generate instances of the GSMA and find the mean and covariance
+    #         of unmodelled modes.
+    fg_alm_list = []
+    for i in range(4):
+        fg_alm_list.append(SM.foreground_gsma_alm_nsidelo(nu=nuarr, lmax=lmax, nside=nside, use_mat_Y=True, delta=delta, seed=123+i))
+    fg_alm_arr = np.array(fg_alm_list)
+    fg_alm_arr = np.array(np.split(fg_alm_arr, len(nuarr), axis=1))
+    fg_alm_unmod_arr  = fg_alm_arr[:,:,Nlmod:]
+
+    # Step 2: Calculate the missing-modes observation matrix.
+    mat_A_unmod = BlockMatrix(mat_A.block[:,:,Nlmod:])
+
+    # Step 3: Compute the data correction and covariance correction.
+    data_corr  = []
+    covar_corr = []
+    for alm_block, mat_A_unmod_block in zip(fg_alm_unmod_arr, mat_A_unmod.block):
+        alm_block_mean = np.mean(alm_block, axis=0)
+        alm_block_cov  = np.cov(alm_block, rowvar=False)
+        data_corr.append(mat_A_unmod_block @ alm_block_mean)
+        covar_corr.append(mat_A_unmod_block @ alm_block_cov @ mat_A_unmod_block.T)
+    data_corr = BlockVector(np.array(data_corr))
+    covar_corr = BlockMatrix(np.array(covar_corr))    
+
+    # Reconstruct the max likelihood estimate of the alm
+    mat_W, cov = MM.calc_ml_estimator_matrix(mat_A=mat_A_mod, mat_N=noise_covar+covar_corr, cov=True)
+    alm_error = np.sqrt(cov.diag)
+    rec_alm = mat_W @ (dnoisy - data_corr)
+    # Compute the chi-square and compare it to the length of the data vector.
+    chi_sq = (dnoisy - mat_A_mod@rec_alm).T @ noise_covar.inv @ (dnoisy - mat_A_mod@rec_alm)
+    chi_sq = sum(chi_sq.diag)
+    print("Chi-square:", chi_sq, "len(data):", dnoisy.vec_len,"+/-", np.sqrt(2*dnoisy.vec_len), "Nparams:", Nlmod*len(nuarr))
+    
+    rec_a00 = np.array(rec_alm.vector[::Nlmod])
+ 
+
+    # Extract the monopole component of the reconstructed alm.
+    a00_error = np.array(alm_error[::Nlmod])
+
+    # Fit the reconstructed a00 component with a polynomial and 21-cm gaussian
+    Npoly = 9
+    fg_mon_p0 = [15, 2.5]
+    fg_mon_p0 += [.001]*(Npoly-2)
+    cm21_mon_p0 = [-0.2, 80, 5]
+    res = curve_fit(f=fg_cm21_polymod, xdata=nuarr, ydata=rec_a00, sigma=a00_error, p0=fg_mon_p0+cm21_mon_p0)
+
+    _plot_results(nuarr, Nlmax, Nlmod, rec_alm.vector, alm_error, fid_alm, cm21_alm, res)
+    del mat_A
+    del mat_A_mod
+    del mat_A_unmod
 
 
 def nontrivial_fg_obs_memopt(ret=False):
