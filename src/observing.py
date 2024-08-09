bd912fc8948db4ca8b863d78369f3e1b715117fb
Yordan Ignatov <dani.ignatov@gmail.com>
2024-08-09 13:54:19 +0100

    added default 21-cm parameters

--- src/observing.py
+++ src/observing.py
@@ -14,6 +14,9 @@ RS = RealSphericalHarmonics()
 Nfreq = 51
 nuarr = np.linspace(50,100,Nfreq)
 
+# Fiducial 21-cm parameters
+cm21_params = [-0.2, 80.0, 5.0]
+
 # Default parameters of the observation and fiducial sky.
 default_pars = {
     "times"  : np.linspace(0,6,3, endpoint=False),
