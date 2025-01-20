f43c7330ccc969a20f4d8e3f20caf269e531604e
Yordan Ignatov <dani.ignatov@gmail.com>
2025-01-20 14:19:57 +0000

    Add 20% basemap error calculations and update plots in plot_basemap_errs function

--- fig_gen.py
+++ fig_gen.py
@@ -291,15 +291,18 @@ def plot_basemap_errs():
     delta_5  = simp_basemap_err_to_delta(5, ref_freq=70)
     delta_10 = simp_basemap_err_to_delta(10, ref_freq=70)
     delta_15 = simp_basemap_err_to_delta(15, ref_freq=70)
-    percentage_err_5  = (nuarr/408)**(-delta_5) - 1
+    delta_20 = simp_basemap_err_to_delta(20, ref_freq=70)
+    #percentage_err_5  = (nuarr/408)**(-delta_5) - 1
     percentage_err_10 = (nuarr/408)**(-delta_10) - 1
-    percentage_err_15 = (nuarr/408)**(-delta_15) - 1
+    #percentage_err_15 = (nuarr/408)**(-delta_15) - 1
+    percentage_err_20 = (nuarr/408)**(-delta_20) - 1
 
     select_freqs = np.array([60,75,90])
     gsma  = SM.foreground_gsma_nsidelo(nu=select_freqs, nside=32)
     _, err_5 = SM.foreground_gsma_alm_nsidelo(nu=select_freqs, lmax=32, nside=32, original_map=True, delta=delta_5)
     _, err_10 = SM.foreground_gsma_alm_nsidelo(nu=select_freqs, lmax=32, nside=32, original_map=True, delta=delta_10)
     _, err_15 = SM.foreground_gsma_alm_nsidelo(nu=select_freqs, lmax=32, nside=32, original_map=True, delta=delta_15)
+    _, err_20 = SM.foreground_gsma_alm_nsidelo(nu=select_freqs, lmax=32, nside=32, original_map=True, delta=delta_20)
     err_mean_5 = []
     for nu, gsma_map in zip(select_freqs, gsma):
         sigma_T   = delta_5 * np.log(408/nu)
@@ -317,20 +320,28 @@ def plot_basemap_errs():
         sigma_T   = delta_15 * np.log(408/nu)
         temp_mean_block = (gsma_map - T_CMB) * np.exp(sigma_T**2/2) + T_CMB
         err_mean_15.append(temp_mean_block)
+    
+    err_mean_20 = []
+    for nu, gsma_map in zip(select_freqs, gsma):
+        sigma_T   = delta_20 * np.log(408/nu)
+        temp_mean_block = (gsma_map - T_CMB) * np.exp(sigma_T**2/2) + T_CMB
+        err_mean_20.append(temp_mean_block)
 
     fig, ax = plt.subplots(1, 2, figsize=(6,2.8))
-    ax[0].plot(nuarr, 1e2*percentage_err_5, label='5%')
+    #ax[0].plot(nuarr, 1e2*percentage_err_5, label='5%')
     ax[0].plot(nuarr, 1e2*percentage_err_10, label='10%')
     #ax[0].plot(nuarr, 1e2*percentage_err_15, label='15%')
+    ax[0].plot(nuarr, 1e2*percentage_err_20, label='20%')
     ax[0].set_xlabel("Frequency [MHz]")
     ax[0].set_ylabel("Approximate Basemap Error [%]")
     ax[0].set_xlim(nuarr[0], nuarr[-1])
-    ax[0].set_ylim(0, 1e2*np.max(percentage_err_10))
+    ax[0].set_ylim(0, 1e2*np.max(percentage_err_20))
 
     bins = np.linspace(-50,50,41)
+    ax[1].hist(1e2*(err_mean_20[0]-err_20[0])/err_mean_20[0], bins=bins, ec='k', alpha=0.6, color='C1', label=' 20%')
     #ax[1].hist(1e2*(err_mean_15[0]-err_15[0])/err_mean_15[0], bins=bins, ec='k', alpha=0.6, color='C2', label=' 15%')
-    ax[1].hist(1e2*(err_mean_10[0]-err_10[0])/err_mean_10[0], bins=bins, ec='k', alpha=0.6, color='C1', label=' 10%')
-    ax[1].hist(1e2*(err_mean_5[0]-err_5[0])/err_mean_5[0], bins=bins, ec='k', alpha=0.6, color='C0', label=' 5%')
+    ax[1].hist(1e2*(err_mean_10[0]-err_10[0])/err_mean_10[0], bins=bins, ec='k', alpha=0.6, color='C0', label=' 10%')
+    #ax[1].hist(1e2*(err_mean_5[0]-err_5[0])/err_mean_5[0], bins=bins, ec='k', alpha=0.6, color='C0', label=' 5%')
     ax[1].set_xlim(-50,50)
     ax[1].set_xlabel("Pixel Temperature Deviation [%]")
     ax[1].set_ylabel("Count")
